7ffdd583b58ae903d86db3ca91fa2a6bff30283d
Charles Melo <melocharles220@gmail.com>
2024-07-27 16:58:12 -0300

    Responsividade

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,13 @@
 import styles from './Post.module.css';
+import {Comment} from './Comment';
+import {Avatar} from './Avatar';
 
 export function Post() {
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/diego3g.png" />
+        <Avatar src="https://github.com/diego3g.png" />
           <div className={styles.authorInfo}>
             <strong>Diego Fernandes</strong>
             <span>Web Developer</span>
@@ -37,6 +39,11 @@ export function Post() {
             <button type="submit">Publicar</button>
           </footer>
       </form>
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
     
   )
--- src/components/Siderbar.jsx
+++ src/components/Siderbar.jsx
@@ -1,6 +1,6 @@
 import styles from './Siderbar.module.css';
 import {PencilLine} from 'phosphor-react'
-
+import { Avatar } from './Avatar';
 export function Siderbar(){
     return(
         <aside className={styles.siderbar}>
@@ -9,7 +9,7 @@ export function Siderbar(){
                 src="https://plus.unsplash.com/premium_photo-1682145730713-34bba6d3d14a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZGV2ZWxvcGVyfGVufDB8fDB8fHww" alt="" />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/charles682.png"/>
+            <Avatar src="https://github.com/maykbrito.png" />
                 <strong>Diego Fernandes</strong>
                 <strong>Web Developer</strong>
             </div>
