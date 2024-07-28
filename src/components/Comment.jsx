05975ffa1d8f8e4a592063c981debbefe75bca2a
Charles Melo <melocharles220@gmail.com>
2024-07-28 09:52:52 -0300

    Removendo Comentarios

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,10 @@ import styles from './Comment.module.css';
 
 import { ThumbsUp, Trash } from 'phosphor-react';
 
-export function Comment() {
+export function Comment({content, onDeleteComment}) {
+    function handleDeleteComment(){
+        onDeleteComment(content)
+    }
     return (
         <div className={styles.comment}>
         <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
@@ -15,12 +18,12 @@ export function Comment() {
                 <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
                 </div>
 
-                <button title="Deletar comentário">
+                <button onClick={handleDeleteComment} title="Deletar comentário">
                 <Trash size={24} />
                 </button>
             </header>
 
-            <p>Muito bom Devon, parabéns!! 👏👏</p>
+            <p>{content}</p>
             </div>
 
             <footer>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,9 +8,10 @@ import {Avatar} from './Avatar';
 import styles from './Post.module.css';
   export function Post({author, publishedAt, content}) {
     const [comments, setComments] = useState([
-      1,
-      2,
+     'Post muito bacana'
     ]);
+
+    const [newCommentText, setNewCommwntText] = useState('')
 
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
       locale: ptBR,
@@ -24,7 +25,20 @@ import styles from './Post.module.css';
     function handleCrateNewComment() {
       event.preventDefault()
   
-      setComments([...comments, comments.length + 1]);
+      setComments([...comments, newCommentText ]);
+      setNewCommwntText('');
+
+    }
+
+    function handleNewCommentChannge(){
+      setNewCommwntText(event.target.value)
+    }
+
+    function deleteComment(commentToDelete){
+      const commentsWithoutDeletedOne = comments.filter(comment =>{
+        return comment != commentToDelete;
+      })
+      setComments(commentsWithoutDeletedOne);
     }
 
   return (
@@ -46,9 +60,9 @@ import styles from './Post.module.css';
       <div className={styles.content}>
       {content.map(line => {
          if (line.type === 'paragraph') {
-          return <p>{line.content}</p>;
+          return <p key={line.content}>{line.content}</p>;
         } else if (line.type === 'link') {
-          return <p><a href="#">{line.content}</a></p>
+          return <p key={line.content}><a href="#">{line.content}</a></p>
         }
       })}
       </div>
@@ -57,7 +71,10 @@ import styles from './Post.module.css';
           <strong>Deixe seu feedback</strong>
 
           <textarea
+            name="comment"
             placeholder="Deixe um comentário"
+            value={newCommentText}
+            onChange={handleNewCommentChannge}
           />
 
           <footer>
@@ -66,7 +83,13 @@ import styles from './Post.module.css';
       </form>
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment/>
+          return(
+              <Comment 
+                key={comment} 
+                content={comment} 
+                onDeleteComment={deleteComment}
+                />
+          )
         })
 
         }
